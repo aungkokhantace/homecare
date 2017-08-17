15dc5218982b0f696a5f69a90e478f4a6c58d17d
Aung Ko Khant <aungkokhantace@gmail.com>
2017-08-17 14:13:55 +0630

    catch skipped issue

--- app/Api/User/UserApiRepository.php
+++ app/Api/User/UserApiRepository.php
@@ -113,6 +113,7 @@ class UserApiRepository implements UserApiRepositoryInterface
                     //Incoming record's updated_at is not later than existing record's updated_at;
                     //So, the record incoming is updated earlier; So, database doesn't need to be updated..
                     else{
+                        dd('this row is skipped',$row);
                         $returnedObj['aceplusStatusCode']       = ReturnMessage::SKIPPED;
                         $returnedObj['aceplusStatusMessage']    = "User data doesn't need to be updated!";
                         $returnedObj['log']                     = $tempLogArr;
